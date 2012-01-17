9030df38a918a46383cd2715040bce6b12463d3e
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-01-17 17:58:06 +0000

    DeviceInfoSettings: change variable ro.build.romversion to ro.modversion for RM support

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -71,7 +71,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
 
         setStringSummary(KEY_FIRMWARE_VERSION, Build.VERSION.RELEASE);
         findPreference(KEY_FIRMWARE_VERSION).setEnabled(true);
-        setValueSummary(KEY_ROM_VERSION, "ro.build.romversion");
+        setValueSummary(KEY_ROM_VERSION, "ro.modversion");
         findPreference(KEY_ROM_VERSION).setEnabled(true);
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
